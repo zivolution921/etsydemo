bf5de9faee6f0ffce72016b3d173c89ca319f01d
Ziv Zamechek <ziv921@yahoo.com>
2016-11-06 12:28:46 -0500

    Add name field to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :name
+  #   devise_parameter_sanitizer.for(:account_update) << :name
+  # end
 end
